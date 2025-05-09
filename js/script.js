bc9cfe6c6c5a6403874f2f23ca6855da60a8d156
Ray <onyangoraybeckham@gmail.com>
2025-05-09 15:56:20 +0300

    Add final project files

---
+++ js/script.js
@@ -0,0 +1,109 @@
+const productContainer = document.querySelector(".product-list");
+const isProductDetailPage = document.querySelector(".product-detail");
+
+if (productContainer) {
+    displayProducts();
+} else if (isProductDetailPage) {
+    displayProductDetail();
+}
+
+function displayProducts(){
+    products.forEach(product => {
+        const productCard = document.createElement("div");
+        productCard.classList.add("product-card");
+        productCard.innerHTML =`
+            <div class="img-box">
+                <img src="${product.colors[0].mainImage}">
+            </div>
+            <h2 class="title">${product.title}</h2>
+            <span class="price">${product.price}</span>
+        `;
+        productContainer.appendChild(productCard);
+
+        const imgBox = productCard.querySelector(".img-box");
+        imgBox.addEventListener("click", () => {
+            sessionStorage.setItem("selectedProduct", JSON.stringify(product));
+            window.location.href="product-detail.html";
+        });
+        
+    });
+}
+
+function displayProductDetail(){
+    const productData = JSON.parse(sessionStorage.getItem("selectedProduct"));
+
+    const titleE1 = document.querySelector(".title");
+    const priceE1 = document.querySelector(".price");
+    const descriptionE1 = document.querySelector(".description");
+    const mainImageContainer = document.querySelector(".main-img");
+    const thumbnailContainer = document.querySelector(".thumbnail-list");
+    const colorContainer = document.querySelector(".color-options");
+    const sizeContainer = document.querySelector(".size-options");
+    const addToCartBtn = document.querySelector("#add-cart-btn");
+
+    let selectedColor = productData.colors[0];
+    let selectedSize = selectedColor.sizes[0];
+
+    function updateProductDisplay(colorData) {
+        if (!colorData.sizes.includes(selectedSize)){
+            selectedSize = colorData.sizes[0];
+
+            mainImageContainer.innerHTML = `<img scr="${colorData.mainImage}">`;
+
+            thumbnailContainer.innerHTML = "";
+            const allThumbnails = [colorData.mainImage].concat(colorData.thumbnails.slice(0, 3));
+            allThumbnails.forEach(thumb =>{
+                const img = document.createElement("img");
+                img.src = thumb;
+
+                thumbnailContainer.appendChild(img);
+
+                img.addEventListener("click", () =>{
+                    mainImageContainer.innerHTML=`<img src="${thumb}">`;
+                });
+            });
+
+        colorContainer.innerHTML = "";
+        productData.colors.forEach(color => {
+            const img = document.createElement("img");
+            img.src = color.mainImage;
+            if(color.name === colorData.name) img.classList.add("selected");
+
+            colorContainer.appendChild(img);
+
+            img.addEventListener("click", () => {
+                selectedColor = color;
+                updateProductDisplay(color);
+            });
+        });
+
+        sizeContainer.innerHTML = "";
+        colorData.sizes.forEach(size => {
+            const btn = document.createElement("button");
+            btn.textContent = size;
+            if (size === selectedSize) btn.classList.add("selected");
+
+            sizeContainer.appendChild(btn);
+
+            btn.addEventListener("click", () => {
+                document.querySelectorAll(".size-options button").forEach(el => el.classList.remove("selected"));
+                btn.classList.add("selected");
+                selectedSize = size;
+            });
+        });
+        }
+
+        titleE1.textContent = productData.title;
+        priceE1.textContext = productData.price;
+        descriptionE1.textContent = productData.description;
+
+        updateProductDisplay(selectedColor);
+
+        addToCartBtn.addEventListener("click", () => {
+            addToCartBtn(productData, selectedColor, selectedSize);
+        });
+
+
+    }
+
+}
